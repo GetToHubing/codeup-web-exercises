f617f831a2517ca2a3f42e362a0d97717fb058e4
David <davidbanker22@yahoo.com>
2023-01-29 22:56:27 -0600

    initial commit for es6 and map-filter-reduce exercises

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,88 @@
+// Create a file named map-filter-reduce.js in your js directory and copy the users data below into it.
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+// Use .map to create an array of strings where each element is a user's email address
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+// Use .reduce to get the longest email from the list of users.
+// Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+let threeLanguageUsers = users.filter(function (n) {
+    return n.languages.length >= 3;
+})
+console.log('#1')
+console.log(threeLanguageUsers)
+
+let usersEmails = users.map(function (n) {
+    let x = [];
+    x.push(n.email);
+    return x;
+});
+console.log('#2')
+console.log(usersEmails);
+
+let totalYears = users.reduce((total, person) => {
+    return total + person.yearsOfExperience;
+}, 0) / users.length;
+console.log('#3')
+console.log(totalYears)
+
+//hopefully this is acceptable
+const longestEmail = function (x) {
+    let longestEmailLength = users.reduce((longest, userEmail) => {
+        if (userEmail.email.length > longest.email.length) {
+            return userEmail;
+        } else {
+            return longest;
+        }
+    });
+    return longestEmailLength.email;
+}
+console.log('#4')
+console.log(longestEmail(users));
+
+
+let nameList = users.reduce((final, names) => {
+    final = final + names.name + ',';
+    return final;
+}, '')
+console.log('#5')
+console.log(nameList);
+
+
+
