6ba39aea2b3ecefcbe9d923c56fb653306e51b53
David <davidbanker22@yahoo.com>
2023-01-30 15:09:28 -0600

    cleaned it up a little bit and finished the bonus question

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -4,6 +4,8 @@
 // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 // Use .reduce to get the longest email from the list of users.
 // Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+//bonus: Use .reduce to get the unique list of languages from the list of users.
+
 const users = [
     {
         id: 1,
@@ -44,23 +46,23 @@ const users = [
 
 let threeLanguageUsers = users.filter(function (n) {
     return n.languages.length >= 3;
-})
-console.log('#1')
-console.log(threeLanguageUsers)
+});
+console.log('#1');
+console.log(threeLanguageUsers);
 
 let usersEmails = users.map(function (n) {
     let x = [];
     x.push(n.email);
     return x;
 });
-console.log('#2')
+console.log('#2');
 console.log(usersEmails);
 
 let totalYears = users.reduce((total, person) => {
     return total + person.yearsOfExperience;
 }, 0) / users.length;
-console.log('#3')
-console.log(totalYears)
+console.log('#3');
+console.log(totalYears);
 
 //hopefully this is acceptable
 const longestEmail = function (x) {
@@ -73,16 +75,25 @@ const longestEmail = function (x) {
     });
     return longestEmailLength.email;
 }
-console.log('#4')
+console.log('#4');
 console.log(longestEmail(users));
 
 
-let nameList = users.reduce((final, names) => {
-    final = final + names.name + ',';
+let nameList = users.reduce((final, user) => {
+    final = final.concat(user.name) +', ';
     return final;
-}, '')
-console.log('#5')
+}, '').slice(0,-2); //definitely not the best way to do it
+console.log('#5');
 console.log(nameList);
 
 
-
+let uniquerList = function (x) {
+    const uniqueList = x.reduce((list, user) => {
+        list.push(...user.languages.filter(language => {
+            return !list.includes(language);
+        }))
+        return list;
+    },[]).join(', ');
+    return uniqueList
+}
+console.log(uniquerList(users));
