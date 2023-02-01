77f8516e1ddbec47a453a65ed105131f1defe77e
David <davidbanker22@yahoo.com>
2023-02-01 14:03:46 -0600

    refactored bonus question

--- js/planets-string.js
+++ js/planets-string.js
@@ -28,13 +28,18 @@
     console.log(x);
 
     //bonus
-    let y = planetsString.split(" "); 
-    let z = y[0].replace(/\|/g, "</li><li>"); // turns the array back into a string for some reason
-    let a = z.split(" ");
-    a.unshift("<ul><li>");
-    a.push("</li></ul>");
-    let b = a.toString();
-    console.log(b);
-    // absolute garbage code...could not get the methods to chain for some reason
+    // let y = planetsString.split(" "); 
+    // let z = y[0].replace(/\|/g, "</li><li>"); // turns the array back into a string for some reason
+    // let a = z.split(" ");
+    // a.unshift("<ul><li>");
+    // a.push("</li></ul>");
+    // let b = a.toString();
+    // console.log(b);
+    // // absolute garbage code...could not get the methods to chain for some reason
 
+
+    let newString = planetsString.split("|").join("</li><li>");
+    let finalString = '<ul><li>'+newString+'</li></ul>';
+    console.log(finalString)
+    
 })();
